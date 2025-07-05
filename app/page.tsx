acd944e81272e18b5be221c5c5c8ca47d775e5b6
BPChance <bradenpchance6@gmail.com>
2025-07-05 14:16:32 -0500

    default signin page

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,16 @@
+import { auth, signIn } from "@/auth";
+import { redirect } from "next/navigation";
+
 export default async function Page() {
+  const session = await auth();
+
+  if (!session) {
+    redirect("/api/auth/signin");
+  }
+
   return (
-    <div className="flex items-center justify-center h-screen">
-      Hello Cinema Guru
+    <div className="flex flex-col items-center justify-center h-screen">
+      Hello {session.user?.email}
     </div>
   );
 }
