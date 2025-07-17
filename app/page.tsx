7d77ebdbd700da77b7938cd9900a8886f498b87f
BPChance <bradenpchance6@gmail.com>
2025-07-17 06:55:59 -0500

    removed redirect

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,5 @@
-import { auth } from "@/auth";
-import { redirect } from "next/navigation";
 import HomePageClient from "@/components/HomePageClient";
 
-export default async function Page() {
-  const session = await auth();
-
-  if (!session) {
-    redirect("/api/auth/signin");
-  }
-
+export default function Page() {
   return <HomePageClient />;
 }
