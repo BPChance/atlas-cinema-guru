91ca0e2129aefc3056729202b71494a8f3d7e7fc
BPChance <bradenpchance6@gmail.com>
2025-07-17 00:15:34 -0500

    changing auth back to original

--- auth.ts
+++ auth.ts
@@ -2,24 +2,21 @@ import NextAuth from "next-auth";
 import GitHub from "next-auth/providers/github";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
+  theme: {
+    brandColor: "#1ED2AF",
+    logo: "/logo.png",
+    buttonText: "#ffffff",
+  },
   providers: [
     GitHub({
-      clientId: process.env.GITHUB_ID!,
-      clientSecret: process.env.GITHUB_SECRET!,
+      clientId: process.env.GITHUB_ID,
+      clientSecret: process.env.GITHUB_SECRET,
     }),
   ],
   callbacks: {
-    async jwt({ token, profile, account }) {
-      if (account?.provider === "github" && profile?.email) {
-        token.email = profile.email;
-      }
-      return token;
-    },
-    async session({ session, token }) {
-      if (token?.email) {
-        session.user.email = token.email as string;
-      }
-      return session;
+    authorized: async ({ auth }) => {
+      // Logged in users are authenticated, otherwise redirect to login page
+      return !!auth;
     },
   },
 });
