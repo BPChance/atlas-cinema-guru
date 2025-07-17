dd9087fc239300406aa4f6bb963dab1d5fcd1d4d
BPChance <bradenpchance6@gmail.com>
2025-07-17 04:22:14 -0500

    removed

--- app/(auth)/favorites/page.tsx
+++
@@ -1,8 +0,0 @@
-import { auth } from "@/auth";
-import FavoritesPageClient from "@/components/FavoritesPageClient";
-
-export default async function Page() {
-  const session = await auth();
-
-  return <FavoritesPageClient userEmail={session?.user?.email ?? ""} />;
-}
