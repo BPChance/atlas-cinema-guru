29a521600bff96ffdca4224677e3dbade0cc4bf2
BPChance <bradenpchance6@gmail.com>
2025-07-16 22:08:00 -0500

    .

--- app/api/favorites/[id]/route.ts
+++ app/api/favorites/[id]/route.ts
@@ -5,35 +5,28 @@ import { auth } from "@/auth";
 /**
  * POST /api/favorites/:id — toggles favorite
  */
-export const POST = auth(
-  //@ts-ignore
-  async (
-    req: NextRequest & { auth?: any },
-    { params }: { params: { id: string } }
-  ) => {
-    const { id } = params;
+export const POST = async (
+  req: NextRequest,
+  { params }: { params: { id: string } }
+) => {
+  const { id } = params;
 
-    //@ts-ignore
-    if (!req.auth) {
-      return NextResponse.json(
-        { error: "Unauthorized - Not logged in" },
-        { status: 401 }
-      );
-    }
-    console.log("AUTH OBJECT:", req.auth);
-
-    const {
-      user: { email }, //@ts-ignore
-    } = req.auth;
-
-    const exists = await favoriteExists(id, email);
-
-    if (exists) {
-      await deleteFavorite(id, email);
-      return NextResponse.json({ message: "Removed from favorites" });
-    } else {
-      await insertFavorite(id, email);
-      return NextResponse.json({ message: "Added to favorites" });
-    }
+  const session = await auth();
+  if (!session?.user?.email) {
+    return NextResponse.json(
+      { error: "Unauthorized - Not logged in" },
+      { status: 401 }
+    );
   }
-);
+
+  const email = session.user.email;
+  const exists = await favoriteExists(id, email);
+
+  if (exists) {
+    await deleteFavorite(id, email);
+    return NextResponse.json({ message: "Removed from favorites" });
+  } else {
+    await insertFavorite(id, email);
+    return NextResponse.json({ message: "Added to favorites" });
+  }
+};
